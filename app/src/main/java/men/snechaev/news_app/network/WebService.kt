bfe3fa37cba26dc037abed6015e0d726b20f1741
SNechaev1 <snechaev1@gmail.com>
2020-05-11 15:42:35 +0300

    fix retrofit request

--- app/src/main/java/men/snechaev/news_app/network/WebService.kt
+++ app/src/main/java/men/snechaev/news_app/network/WebService.kt
@@ -1,12 +1,18 @@
 package men.snechaev.news_app.network
 
-import androidx.lifecycle.LiveData
+import men.snechaev.news_app.data.NewsRaw
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface WebService {
 
-    @GET("v2/everything?q=android&from=2019-04-00&sortBy=publishedAt&apiKey=26eddb253e7840f988aec61f2ece2907&page={page}")
-    suspend fun getNews(@Path("page") id: Long): NewsJson
+    @GET("v2/everything")
+    suspend fun getNews(
+        @Query("q") q: String = "android",
+        @Query("from") from: String = "2019-04-00",
+        @Query("sortBy") sortBy: String = "publishedAt",
+        @Query("apiKey") apiKey: String = "26eddb253e7840f988aec61f2ece2907",
+        @Query("page") page: Int
+    ): NewsRaw
 
 }
