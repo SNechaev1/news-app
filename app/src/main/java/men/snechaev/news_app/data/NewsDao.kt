1852726eb67fe793ceb0d274b6e83fa66dfb1916
SNechaev1 <snechaev1@gmail.com>
2020-05-13 22:14:43 +0300

    add elements for paging list

--- app/src/main/java/men/snechaev/news_app/data/NewsDao.kt
+++ app/src/main/java/men/snechaev/news_app/data/NewsDao.kt
@@ -1,15 +1,22 @@
 package men.snechaev.news_app.data
 
+import androidx.paging.DataSource
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import men.snechaev.news_app.data.News
 
 @Dao
 interface NewsDao {
+
+    // The Int type parameter tells Room to use a PositionalDataSource
+    // object, with position-based loading under the hood.
     @Query("SELECT * FROM news")
-    fun getAll(): List<News>
+    fun getAll(): DataSource.Factory<Int, News>
+
+    @Query("SELECT * FROM news WHERE id = :id")
+    fun getById(id: Int): DataSource.Factory<Int, News>
+
 
     @Query("SELECT * FROM news WHERE page = :page")
     fun getByPage(page: Int): List<News>
