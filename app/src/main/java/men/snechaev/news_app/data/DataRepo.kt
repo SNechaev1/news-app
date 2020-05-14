a17c4ac91add27f9a9dfc3df2fd3c50e1e22d248
SNechaev1 <snechaev1@gmail.com>
2020-05-14 21:33:10 +0300

    move code from boundarycallback to datarepo

--- app/src/main/java/men/snechaev/news_app/data/DataRepo.kt
+++ app/src/main/java/men/snechaev/news_app/data/DataRepo.kt
@@ -1,6 +1,13 @@
 package men.snechaev.news_app.data
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import men.snechaev.news_app.network.NetStatus
 import men.snechaev.news_app.network.NewsRaw
 import men.snechaev.news_app.network.WebService
 import javax.inject.Inject
@@ -13,17 +20,63 @@ constructor(
     val newsDao: NewsDao
 ) {
 
+    private val job = Job()
+    private val scope = CoroutineScope(Dispatchers.IO + job)
+
+    private val _networkErrors = MutableLiveData<Boolean>()
+
+    // LiveData of network errors.
+    val networkErrors: LiveData<Boolean>
+        get() = _networkErrors
+
+    // keep the last requested page. When the request is successful, increment the page number.
+    // not used, because right now page saved in db(not optimal, but easy to use)
+    private var lastRequestedPage = 1
+
+    fun requestAndSaveNews(loadSize: Int = 1, startPage: Int = 1) {
+        if (!NetStatus.isConnected) {
+            Log.d("net", "notConnected")
+            return
+        }
+//        if (job.isActive) return
+        scope.launch {
+            try {
+                // because range inclusive, 5..5 loaded 5; 5..6 loaded 5 and 6,
+                // we need decrement endPage by one
+                val endPage = startPage + loadSize - 1
+                for (pageCount in startPage..endPage) {
+                    val response = webService.getNews(page = pageCount)
+                    when {
+                        response.isSuccessful -> {
+                            val articles = response.body()?.articles
+                            val newsList =
+                                articles?.let { convertNewsRawToNews(page = 1, articles = it) }
+                            newsList?.let { insertNewsIntoDb(it) }
+                        }
+                    }
+                }
+
+            } catch (exception: Exception) {
+                Log.e("NewsDataSource", exception.message.orEmpty())
+            }
+        }
+    }
+
     // we need only several field from news article
-    fun convertNewsRawToNews(page: Int, newsRaw: NewsRaw): List<News> {
+    fun convertNewsRawToNews(page: Int, articles: List<NewsRaw.Article>): MutableList<News> {
         val newsList: MutableList<News> = mutableListOf()
-        for(article in newsRaw.articles!!) {
+        for (article in articles) {
 //            Log.d("article", article.toString())
-            val news: News = News(page = page, title = article.title, description = article.description,
-                urlToImage = article.urlToImage, publishedAt = article.publishedAt, url = article.url)
+            val news = News(
+                page = page,
+                title = article.title,
+                description = article.description,
+                urlToImage = article.urlToImage,
+                publishedAt = article.publishedAt,
+                url = article.url
+            )
             newsList.add(news)
         }
-        insertNewsIntoDb(newsList)
-        Log.d("db", getNewsByPageFromDb(1).toString())
         return newsList
     }
 
--- app/src/main/java/men/snechaev/news_app/data/NewsBoundaryCallback.kt
+++ app/src/main/java/men/snechaev/news_app/data/NewsBoundaryCallback.kt
@@ -1,15 +1,7 @@
 package men.snechaev.news_app.data
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.paging.PagedList
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import men.snechaev.news_app.network.NewsRaw
-import men.snechaev.news_app.network.WebService
 import javax.inject.Inject
 
 /**
@@ -18,95 +10,19 @@ import javax.inject.Inject
  */
 class NewsBoundaryCallback @Inject
 constructor(
-    val webService: WebService,
-    val newsDao: NewsDao
+    val dataRepo: DataRepo
 ) : PagedList.BoundaryCallback<News>() {
-
-    companion object {
-        private const val NETWORK_PAGE_SIZE = 50
-    }
-
-    private val job = Job()
-    private val scope = CoroutineScope(Dispatchers.IO + job)
-
-    // keep the last requested page. When the request is successful, increment the page number.
-    // not used, because right now page saved in db(not optimal, but easy to use)
-    private var lastRequestedPage = 1
-
-    private val _networkErrors = MutableLiveData<String>()
-    // LiveData of network errors.
-    val networkErrors: LiveData<String>
-        get() = _networkErrors
-
-    // avoid triggering multiple requests in the same time
-    private var isRequestInProgress = false
-
 
     // Database returned 0 items. We should query the backend for more items.
     override fun onZeroItemsLoaded() {
         Log.d("RepoBoundaryCallback", "onZeroItemsLoaded")
-        requestAndSaveNews(loadSize = 5)
+        dataRepo.requestAndSaveNews(loadSize = 1)
     }
 
-
-     // When all items in the database were loaded, we need to query the backend for more items.
+    // When all items in the database were loaded, we need to query the backend for more items.
     override fun onItemAtEndLoaded(itemAtEnd: News) {
-         Log.d("RepoBoundaryCallback", "onItemAtEndLoaded")
-
-         requestAndSaveNews(loadSize = 1, startPage = itemAtEnd.page + 1)
+        Log.d("RepoBoundaryCallback", "onItemAtEndLoaded")
+        dataRepo.requestAndSaveNews(loadSize = 1, startPage = itemAtEnd.page + 1)
     }
 
-
-    fun requestAndSaveNews(loadSize: Int = 1, startPage: Int = 1 ) {
-        if (isRequestInProgress) return
-
-        isRequestInProgress = true
-        scope.launch {
-            try {
-                // because range inclusive, 5..5 loaded 5; 5..6 loaded 5 and 6,
-                // we need decrement loadSize by one
-                val endPage = startPage + loadSize - 1
-                for (pageCount in startPage..endPage) {
-                    val response = webService.getNews(page = pageCount)
-                    when {
-                        response.isSuccessful -> {
-                            val articles = response.body()?.articles
-                            val newsList = articles?.let { convertNewsRawToNews(page = 1, articles = it) }
-                            newsList?.let { insertNewsIntoDb(it) }
-                            isRequestInProgress = false
-                        }
-                    }
-                }
-
-            } catch (exception : Exception){
-                Log.e("NewsDataSource", exception.message.orEmpty())
-                isRequestInProgress = false
-            }
-
-        }
-    }
-
-    // we need only several field from news article
-    fun convertNewsRawToNews(page: Int, articles: List<NewsRaw.Article>): MutableList<News> {
-        val newsList: MutableList<News> = mutableListOf()
-        for(article in articles) {
-//            Log.d("article", article.toString())
-            val news = News(page = page, title = article.title, description = article.description,
-                urlToImage = article.urlToImage, publishedAt = article.publishedAt, url = article.url)
-            newsList.add(news)
-        }
-        return newsList
-    }
-
-    fun insertNewsIntoDb(list: List<News>) {
-        newsDao.insertAll(list)
-    }
-
-//    fun getNewsFromDb(): List<News> {
-//        return newsDao.getAll()
-//    }
-
-    fun getNewsByPageFromDb(page: Int): List<News> {
-        return newsDao.getByPage(page)
-    }
 }
