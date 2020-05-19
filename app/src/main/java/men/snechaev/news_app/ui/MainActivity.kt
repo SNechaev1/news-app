1db3ad0a1d44f6f49318a10ce8b1ee235afda57a
SNechaev1 <snechaev1@gmail.com>
2020-05-19 08:26:17 +0300

    clean up

--- app/src/main/java/men/snechaev/news_app/ui/MainActivity.kt
+++ app/src/main/java/men/snechaev/news_app/ui/MainActivity.kt
@@ -3,18 +3,12 @@ package men.snechaev.news_app.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import men.snechaev.news_app.R
-import men.snechaev.news_app.data.News
-import men.snechaev.news_app.ui.newslist.NewsFragment
 
-class MainActivity : AppCompatActivity(), NewsFragment.OnListFragmentInteractionListener {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    override fun onListFragmentInteraction(item: News?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 }
